def3fe2f5b42df81e17d1abdbc899f0ac5bde26a
Ryan Whitmire <ryanw51@gmail.com>
2020-01-17 10:11:59 -0500

    Revert "remove notarize process for now"
    This reverts commit e31a359422ad3dfde38f4a828b41695533c07bec.

---
+++ scripts/notarize.js
@@ -0,0 +1,21 @@
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+
+  // don't early return when we have notarization process configured.
+  return;
+
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: 'com.velaro.chat',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS,
+  });
+};
